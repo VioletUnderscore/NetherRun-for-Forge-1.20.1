d2fe0ab8940eabdc99e241bebe1840ae19382d50
VioletUnderscore <violetunderscore@gmail.com>
2024-06-02 18:19:25 -0600

    m1d2-1
    +Added "Netherrun Pickaxe" (with no functionality)
    +Added a recipe for "Block of Go Up"
    +Removed "test" item

--- src/main/java/net/violetunderscore/netherrun/item/ModItems.java
+++ src/main/java/net/violetunderscore/netherrun/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.violetunderscore.netherrun.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,8 +12,9 @@ import net.violetunderscore.netherrun.NetherRun;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, NetherRun.MODID);
-    public static final RegistryObject<Item> TEST = ITEMS.register("test",
-            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NETHERRUN_PICKAXE = ITEMS.register("netherrun_pickaxe",
+            () -> new PickaxeItem(Tiers.NETHERITE, -4, -2.8f, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
