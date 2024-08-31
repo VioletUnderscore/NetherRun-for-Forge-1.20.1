24fae24f0fa868a480b4a141caa26ba4b5794947
VioletUnderscore <violetunderscore@gmail.com>
2024-08-30 19:05:29 -0600

    m3d30-2
    +Added name for soul slime block in en_us.json
    +Removed debug feature to place crying obsidian platform when right-clicking with a netherrun totem

--- src/main/java/net/violetunderscore/netherrun/item/custom/NetherrunTotemItem.java
+++ src/main/java/net/violetunderscore/netherrun/item/custom/NetherrunTotemItem.java
@@ -31,49 +31,6 @@ public class NetherrunTotemItem extends Item {
         super(pProperties);
     }
 
-    /*public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        if (pStack.getTag().getLong("netherrun.enchant_timeout") <= pLevel.getGameTime()
-                && pStack.getEnchantmentLevel(Enchantments.BLOCK_EFFICIENCY) == 10)
-        {
-            Map<Enchantment, Integer> pEnchantments = EnchantmentHelper.getEnchantments(pStack);
-
-            pEnchantments.remove(Enchantments.BLOCK_EFFICIENCY);
-            EnchantmentHelper.setEnchantments(pEnchantments, pStack);
-        }
-        else if (pStack.getTag().getLong("netherrun.enchant_timeout") > pLevel.getGameTime()
-                && pStack.getEnchantmentLevel(Enchantments.BLOCK_EFFICIENCY) != 10)
-        {
-            pStack.enchant(Enchantments.BLOCK_EFFICIENCY, 10);
-        }
-        if (pStack.getTag().getLong("netherrun.enchant_ready_timeout") <= pLevel.getGameTime()) {
-            pStack.getTag().putBoolean("netherrun.enchant_ready", true);
-            pStack.getTag().putInt("CustomModelData", 0);
-        }
-    }*/
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        /*if (!pLevel.isClientSide() && pPlayer.getMainHandItem().getTag().getLong("netherrun.enchant_ready_timeout") <= pLevel.getGameTime()
-                && pPlayer.getMainHandItem().getItem().toString().equals(ModItems.NETHERRUN_PICKAXE.get().toString()))
-        {
-            pPlayer.getMainHandItem().getTag().putBoolean("netherrun.enchant_ready", false);
-            pPlayer.getMainHandItem().getTag().putInt("CustomModelData", 1);
-            pPlayer.getMainHandItem().getTag().putLong("netherrun.enchant_timeout", pLevel.getGameTime() + 100);
-            pPlayer.getMainHandItem().getTag().putLong("netherrun.enchant_ready_timeout", pLevel.getGameTime() + 600);
-        }*/
-        if (!pLevel.isClientSide()) {
-            if (pLevel instanceof ServerLevel serverLevel) {
-                for (int xValue = -1; xValue <= 1; xValue += 1) {
-                    for (int zValue = -1; zValue <= 1; zValue += 1) {
-                        BlockPos pos = BlockPos.containing(pPlayer.position().x() + xValue, pPlayer.position().y() - 1, pPlayer.position().z() + zValue);
-                        pLevel.setBlock(pos, Blocks.CRYING_OBSIDIAN.defaultBlockState(), 3);
-                        NetworkHandler.sendToAllPlayers(new NetherrunTotemPlaceBlockPacket(pos, Blocks.CRYING_OBSIDIAN.defaultBlockState()), serverLevel.getServer());
-                    }
-                }
-            }
-        }
-        return super.use(pLevel, pPlayer, pUsedHand);
-    }
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
 
         if (pStack.getOrCreateTag().getLong("netherrun.ready_timeout") <= pLevel.getGameTime()) {
